d556404c5d3cd493b368760165c4b999b0fb072e
L91519 <jennifer045020@gmail.com>
2020-09-26 22:50:51 +0900

    Gson Factory 수정

--- app/src/main/java/com/example/github_api_application/api/GithubGsonConverterFactory.kt
+++ app/src/main/java/com/example/github_api_application/api/GithubGsonConverterFactory.kt
@@ -6,16 +6,27 @@ import retrofit2.Retrofit
 import java.lang.reflect.Type
 
 class GithubGsonConverterFactory : Converter.Factory() {
-    override fun responseBodyConverter(type: Type, annotations: Array<out Annotation>?, retrofit: Retrofit?): Converter<ResponseBody, *>? {
-        if (type == String::class.java) {
-            return StringResponseBodyConverter
+
+    override fun responseBodyConverter(type: Type, annotations: Array<Annotation>, retrofit: Retrofit): Converter<ResponseBody, String>? {
+        return if(type == String::class.java) {
+            Converter {response ->
+                response.string()
+            }
+        } else {
+            null
         }
-        return null
     }
+
+//    override fun responseBodyConverter(type: Type, annotations: Array<out Annotation>?, retrofit: Retrofit?): Converter<ResponseBody, *>? {
+//        if (type == String::class.java) {
+//            return StringResponseBodyConverter
+//        }
+//        return null
+//    }
 }
 
-object StringResponseBodyConverter : Converter<ResponseBody, String> {
-    override fun convert(value: ResponseBody): String {
-        return value.string()
-    }
-}
+//object StringResponseBodyConverter : Converter<ResponseBody, String> {
+//    override fun convert(value: ResponseBody): String {
+//        return value.string()
+//    }
+//}
